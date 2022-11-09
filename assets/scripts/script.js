1e218a1bc70eca72d4ef59e15913616184388170
loudwhisperer <driftaway44@gmail.com>
2022-11-09 16:45:03 -0700

    added comments to script.js and took out comments from initial html file

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,5 +1,6 @@
 
 $(function () {
+  //global variables
   const currentD = $("#currentDay");
   const save = $(".saveBtn");
   const one = $("#hour-one");
@@ -11,27 +12,25 @@ $(function () {
   const ten = $("#hour-ten");
   const eleven = $("#hour-eleven");
   const twelve = $("#hour-twelve");
-  
-  let heroInterval = setInterval(function () {
+  let now = dayjs().format("H");
+  let varArr = [one, two, three, four, five, nine, ten, eleven, twelve];
+  let timeArr = [13, 14, 15, 16, 17, 9, 10, 11, 12];
+  // adds current date and day to header
+  setInterval(function () {
     currentD.text(dayjs().format("dddd, MMMM D YYYY"));
   }, 1000);
-
+//save click event
   function saveManager(event) {
     event.preventDefault();
+    //set local variables
     let text = $(this).siblings("textarea").val();
     let time = $(this).siblings("div").data("mili");
-
+    // save local variables to storage
     localStorage.setItem(time, text);
   }
 
   save.on("click", saveManager);
-  
-  let now = dayjs().format("H")
-
-
-  let varArr = [one, two, three, four, five, nine, ten, eleven, twelve]
-  let timeArr = [13, 14, 15, 16, 17, 9, 10, 11, 12]
-
+// setting color of each time and getting local storage values 
   for (let i = 0; i < varArr.length; i++){
      for (let i = 0; i < timeArr.length; i++){
     if (now == timeArr[i]){
